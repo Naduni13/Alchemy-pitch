3deb5e01de9509d3d4eac470d3c6bf792e3c68c0
Naduni Peiris <peirisnaduni13@gmail.com>
2026-04-03 11:31:14 +0530

    fonts added

--- alchemy-pitch/app/home/page.tsx
+++ alchemy-pitch/app/home/page.tsx
@@ -1,16 +1,20 @@
+// app/layout.tsx or page.tsx
+import { Poppins } from 'next/font/google'
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+})
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
-      
-
+    <main className={`min-h-screen bg-gray-50 ${poppins.className}`}>
       <section className="max-w-7xl mx-auto px-6 py-24 text-center">
-        <h1 className="text-5xl md:text-7xl font-bold leading-tight">
-          Build Beautiful Websites
+        <h1 className="font-myfont text-5xl font-bold">
+          Build Beautiful Websites & great user experience with Next.js, TypeScript and Tailwind CSS
         </h1>
 
-        <p className="mt-6 text-lg text-gray-600 max-w-2xl mx-auto">
+        <p className="mt-6 text-lg text-gray-600 max-w-2xl mx-auto font-poppins">
           Create fast, modern and responsive websites using Next.js,
           TypeScript and Tailwind CSS.
         </p>
@@ -20,5 +24,5 @@ export default function Home() {
         </button>
       </section>
     </main>
-  );
+  )
 }
